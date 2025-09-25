3214f535e4f79876742f2a3a59847a6800f22cff
Rishabh Gautam <rishabhgautam8292@gmail.com>
2025-09-26 00:32:34 +0530

    Add files via upload

---
+++ src/main/java/com/security/bank/repository/CardRepository.java
@@ -0,0 +1,13 @@
+package com.security.bank.repository;
+
+import com.security.bank.entity.Card;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CardRepository extends JpaRepository<Card,Long> {
+
+    Optional<Card> findByCardNumber(Long cardNumber);
+}
